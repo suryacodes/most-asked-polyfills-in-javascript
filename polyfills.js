585b7cdc3468e518c695a1b3e0b2d8ee5a6e322e
suryacodes <62347567+suryacodes@users.noreply.github.com>
2024-12-03 07:05:57 +0530

    filter,  reduce, forEach

--- polyfills.js
+++ polyfills.js
@@ -36,3 +36,36 @@ Array.prototype.myMap = function (callback, thisArg) {
     }
     return newArr;
 };
+
+//filter
+Array.prototype.myFilter = function(callback) {
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      arr.push(this[i]);
+    }
+  }
+  return arr;
+};
+
+
+//reduce
+Array.prototype.myReduce = function (callback, initialVal) {
+    // If initialVal is provided, use it; otherwise, take the first element of the array
+    let result = initialVal !== undefined ? initialVal : this[0];
+    let startIndex = initialVal !== undefined ? 0 : 1;
+
+    for (let index = startIndex; index < this.length; index++) {
+        result = callback(result, this[index], index, this);
+    }
+    return result;
+};
+
+//forEach
+Array.prototype.myForEach = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
+
+
